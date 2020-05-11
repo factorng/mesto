a984b299f7728562a05327b8cb93e8ce9e78873c
Aleksey Efimov <factorng@yandex.ru>
2020-05-11 12:30:07 +0300

    4 attempt

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,11 @@ const close_btn = document.querySelector('.edit-profile__button-close');
 const form = document.querySelector('.edit-profile__form');
 const save_btn = document.querySelector('.edit-profile__button-submit');
 const edit_window = document.querySelector('.edit-profile');
-const profile_name = this.document.querySelector('.profile__name');
-const profile_occupation = this.document.querySelector('.profile__occupation');
-const input_name = this.document.querySelector('.edit-profile__input-name');
-const input_occupation = this.document.querySelector('.edit-profile__input-occupation');
-const edit_btn = this.document.querySelector('.profile__edit-button');
+const profile_name = document.querySelector('.profile__name');
+const profile_occupation = document.querySelector('.profile__occupation');
+const input_name = document.querySelector('.edit-profile__input-name');
+const input_occupation = document.querySelector('.edit-profile__input-occupation');
+const edit_btn = document.querySelector('.profile__edit-button');
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -17,11 +17,12 @@ function formSubmitHandler(evt) {
 form.addEventListener('submit', formSubmitHandler);
 
 function toggleForm() {
+  if(!edit_window.classList.contains('edit-profile_open')) {
+    input_name.value = profile_name.textContent;
+    input_occupation.value = profile_occupation.textContent;
+  }
   edit_window.classList.toggle('edit-profile_open');
 }
 
 edit_btn.addEventListener('click', toggleForm);
-close_btn.addEventListener('click', function () {
-  form.reset();
-  toggleForm();
-});
+close_btn.addEventListener('click', toggleForm);
