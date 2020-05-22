d30f76ffcced1427dc9208916f1380d7d8cd0c84
Aleksey Efimov <factorng@yandex.ru>
2020-05-22 14:17:59 +0300

    final release 1.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,121 +1,142 @@
-const close_btn = document.querySelector('.popup__button-close');
-const form = document.querySelector('.edit-profile__form');
-const save_btn = document.querySelector('.edit-profile__button-submit');
-const edit_window = document.querySelector('.edit-profile');
-const profile_name = document.querySelector('.profile__name');
-const profile_occupation = document.querySelector('.profile__occupation');
-const input_name = document.querySelector('.edit-profile__input-name');
-const input_occupation = document.querySelector('.edit-profile__input-occupation');
-const edit_btn = document.querySelector('.profile__edit-button');
+const editProfile = {
+  close_btn: document.querySelector('.popup__button-close'),
+  edit_btn: document.querySelector('.profile__edit-button'),
+  save_btn: document.querySelector('.edit-profile__button-submit'),
+  edit_window: document.querySelector('.edit-profile'),
+  profile_name: document.querySelector('.profile__name'),
+  profile_occupation: document.querySelector('.profile__occupation'),
+  input_name: document.querySelector('.edit-profile__input-name'),
+  input_occupation: document.querySelector('.edit-profile__input-occupation'),
+  form: document.querySelector('.edit-profile__form'),
+  formSubmitHandler: function(evt) {
+    evt.preventDefault();
+    editProfile.profile_name.textContent = editProfile.input_name.value;
+    editProfile.profile_occupation.textContent = editProfile.input_occupation.value;
+    editProfile.formToggle();
+  },
+  formToggle: function() {
+    if(!editProfile.edit_window.classList.contains('popup_open')) {
+      editProfile.input_name.value = editProfile.profile_name.textContent;
+      editProfile.input_occupation.value = editProfile.profile_occupation.textContent;
+    }
+    editProfile.edit_window.classList.toggle('popup_open');
+  },
+};
+editProfile.form.addEventListener('submit', editProfile.formSubmitHandler);
+editProfile.edit_btn.addEventListener('click', editProfile.formToggle);
+editProfile.close_btn.addEventListener('click', editProfile.formToggle);
 
-const addCardForm = document.querySelector('.add-card__form');
-const addCardForm_window = document.querySelector('.add-card');
-const addCardForm_add_btn = document.querySelector('.profile__add-button');
-const addCardForm_close_btn = document.querySelector('.add-card__button-close');
-const cardImageWrapper = document.querySelector('.places');
-const cardImageTemplate = document.querySelector('#cardPlace').content;
-const addCardInputName = document.querySelector('.add-card__input-name');
-const addCardInputLink = document.querySelector('.add-card__input-link');
-const showPhotoWindow = document.querySelector('.show-photo');
-const showPhotoWindow_close_btn = document.querySelector('.show-photo__button-close');
-const initialCards = [
-  {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+const addCard = {
+  form: document.querySelector('.add-card__form'),
+  window: document.querySelector('.add-card'),
+  add_btn: document.querySelector('.profile__add-button'),
+  close_btn: document.querySelector('.add-card__button-close'),
+  cardImageWrapper: document.querySelector('.places'),
+  cardImageTemplate: document.querySelector('#cardPlace').content,
+  inputName: document.querySelector('.add-card__input-name'),
+  inputLink: document.querySelector('.add-card__input-link'),
+  initialCards: [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+        like: 0
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+        like: 0
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+        like: 0
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+        like: 0
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+        like: 0
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+        like: 0
+    }
+  ],
+  formSubmitHandler: function(evt) {
+    evt.preventDefault();
+    let addObj = {};
+    addObj.name = addCard.inputName.value;
+    addObj.link = addCard.inputLink.value;
+    addObj.like = 0;
+    addCard.initialCards.unshift(addObj);
+    addCard.toggleForm();
+    addCard.showCards();
+    addCard.inputName.value = '';
+    addCard.inputLink.value = '';
   },
-  {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  toggleForm: function() {
+    addCard.window.classList.toggle('popup_open');
   },
-  {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  showCards: function() {
+    addCard.cardImageWrapper.querySelectorAll('*').forEach(elem => elem.remove());
+    addCard.initialCards.forEach((elem) => {
+      let cardImage = addCard.cardImageTemplate.cloneNode(true);
+      cardImage.querySelector('.place__title').innerText = elem.name;
+      cardImage.querySelector('.place__image').alt = elem.name;
+      cardImage.querySelector('.place__image').src = elem.link;
+      if(elem.like == 1){
+        cardImage.querySelector('.place__button-like').className += ' place__button-like_active';
+      }
+      addCard.cardImageWrapper.append(cardImage);
+    });
   },
-  {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+};
+addCard.form.addEventListener('submit', addCard.formSubmitHandler);
+addCard.add_btn.addEventListener('click', addCard.toggleForm);
+addCard.close_btn.addEventListener('click', addCard.toggleForm);
+
+
+const showPhoto = {
+  close_btn: document.querySelector('.show-photo__button-close'),
+  window: document.querySelector('.show-photo'),
+  showPhoto: function(evt) {
+    showPhoto.window.classList.toggle('show-photo_active');
+    showPhoto.window.querySelector('.show-photo__image').src = evt.target.src;
+    showPhoto.window.querySelector('.show-photo__title').innerText = evt.target.parentElement.querySelector('.place__title').innerText;
   }
-];
+};
+ showPhoto.close_btn.addEventListener('click', (evt) => {
+  showPhoto.window.classList.toggle('show-photo_active');
+ });
 
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  profile_name.textContent = input_name.value;
-  profile_occupation.textContent = input_occupation.value;
-  toggleForm();
-}
-form.addEventListener('submit', formSubmitHandler);
-
-function toggleForm() {
-  if(!edit_window.classList.contains('popup_open')) {
-    input_name.value = profile_name.textContent;
-    input_occupation.value = profile_occupation.textContent;
-  }
-  edit_window.classList.toggle('popup_open');
-}
-edit_btn.addEventListener('click', toggleForm);
-close_btn.addEventListener('click', toggleForm);
-
-function addCardFormSubmitHandler(evt) {
-  evt.preventDefault();
-  let addObj = {};
-  addObj.name = addCardInputName.value;
-  addObj.link = addCardInputLink.value;
-  initialCards.unshift(addObj);
-  toggleAddCardForm();
-  showCards();
-  addCardInputName.value = '';
-  addCardInputLink.value = '';
-}
-addCardForm.addEventListener('submit', addCardFormSubmitHandler);
-
-function toggleAddCardForm() {
-   addCardForm_window.classList.toggle('popup_open');
-}
-
-function showCards() {
-  cardImageWrapper.querySelectorAll('*').forEach(elem => elem.remove());
-  initialCards.forEach((elem) => {
-    let cardImage = cardImageTemplate.cloneNode(true);
-    cardImage.querySelector('.place__title').innerText = elem.name;
-    cardImage.querySelector('.place__image').alt = elem.name;
-    cardImage.querySelector('.place__image').src = elem.link;
-    cardImageWrapper.append(cardImage);
-  });
-}
-addCardForm_add_btn.addEventListener('click', toggleAddCardForm);
-addCardForm_close_btn.addEventListener('click', toggleAddCardForm);
-cardImageWrapper.addEventListener('click', (evt) => {
+ function toggleLike(evt) {
+   evt.target.classList.toggle('place__button-like_active');
+   addCard.initialCards.forEach((elem, i, arr) => {
+    if(elem.name == evt.target.parentElement.querySelector('.place__title').innerText) {
+      elem.like = 1;
+    }
+   });
+ }
+ addCard.cardImageWrapper.addEventListener('click', (evt) => {
   if(evt.target.className == 'place__button-delete') {
     evt.target.parentElement.remove();
-    let cardToDelete = initialCards.find((el) => el.name == evt.target.parentElement.querySelector('.place__title').innerText);
-    initialCards.splice(initialCards.indexOf(cardToDelete), 1);
+    let cardToDelete = addCard.initialCards.find((el) => el.name == evt.target.parentElement.querySelector('.place__title').innerText);
+    addCard.initialCards.splice(addCard.initialCards.indexOf(cardToDelete), 1);
   }
   if(evt.target.classList.contains('place__button-like')) {
     toggleLike(evt);
+    console.log(evt.target.parentElement.querySelector('.place__title').innerText);
   }
   if(evt.target.className == 'place__image') {
-    showPhoto(evt);
+    showPhoto.showPhoto(evt);
   }
 });
-showPhotoWindow_close_btn.addEventListener('click', (evt) => {
-  showPhotoWindow.classList.toggle('show-photo_active');
-});
-function toggleLike(evt) {
-  evt.target.classList.toggle('place__button-like_active');
-}
-function showPhoto(evt) {
-  showPhotoWindow.classList.toggle('show-photo_active');
-  showPhotoWindow.querySelector('.show-photo__image').src = evt.target.src;
-  showPhotoWindow.querySelector('.show-photo__title').innerText = evt.target.parentElement.querySelector('.place__title').innerText;
-}
+
 window.onload = function() {
-  showCards();
+  addCard.showCards();
 };
